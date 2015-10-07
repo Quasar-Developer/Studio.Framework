65a1511bbdbae4ac34fea8425be6528b422aff84
송광호 <zaneiru@hotmail.com>
2015-10-08 03:29:04 +0900

    프레임웍 솔루션 파일 추가

---
+++ Studio.Framework.Database/MongoDB/MongoManager.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+
+namespace Studio.Framework.Database
+{
+    public class MongoManager<T>
+    {
+        protected IMongoClient _client;
+        protected IMongoDatabase _dataBase;
+
+        public MongoDBManager()
+        {
+            MongoCredential credential = MongoCredential.CreateMongoCRCredential(databaseName, username, )
+            MongoClientSettings settings = new MongoClientSettings
+            {
+                Credentials = new[] {  },
+            };
+            
+            _client = new MongoClient(settings);
+        }
+    }
+}
